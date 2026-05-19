68f10ed9ec54b60ac0fea5b11f35979941828983
Lucas Santamaria <lucas.santamaria@utp.ac.pa>
2026-05-19 08:43:48 -0500

    feat: create RequestStatus entity

--- backend/src/request-statuses/request-statuses.module.ts
+++ backend/src/request-statuses/request-statuses.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RequestStatus } from './entities/request-status.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([RequestStatus])],
+})
 export class RequestStatusesModule {}
