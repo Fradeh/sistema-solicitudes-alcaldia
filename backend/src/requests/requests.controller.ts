a8f133bdcba96cf9ff313393a82dd3b3123ede28
Saul <saul.abrego@utp.ac.pa>
2026-05-19 21:59:11 -0500

    feat(backend): crear endpoint para consultar documentos por solicitud #48

--- backend/src/requests/requests.controller.ts
+++ backend/src/requests/requests.controller.ts
@@ -2,14 +2,15 @@ import { Controller, Post, Body, Get, Param } from '@nestjs/common';
 import { RequestsService } from './requests.service';
 import { CreateDocumentDto } from './dto/create-document.dto';
 import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { DocumentUser } from '../documents/schema/document-user.schema'; 
 
-@ApiTags('Requests & Documents') // 🌟 Esto los agrupa bonito en Swagger UI
+@ApiTags('Requests & Documents')
 @Controller('requests')
 export class RequestsController {
   constructor(private readonly requestsService: RequestsService) {}
 
   /**
-   *  POST /requests/documents Registra la metadata simulada en Mongo
+   * POST /requests/documents
    */
   @Post('documents')
   @ApiOperation({ summary: 'Registrar la metadata de un documento (MongoDB)' })
@@ -22,7 +23,13 @@ export class RequestsController {
    * GET /requests/:id/documents
    */
   @Get(':id/documents')
-  @ApiOperation({ summary: 'Consultar el expediente de documentos de una solicitud' })
+  @ApiOperation({ summary: 'Consultar el expediente de documentos de una solicitud específica' })
+  @ApiResponse({ 
+    status: 200, 
+    description: 'Lista de documentos asociados a la solicitud obtenida con éxito.',
+    type: [DocumentUser] // swagger mostrará los campos (fileName, url, etc.) al Frontend
+  })
+  @ApiResponse({ status: 404, description: 'Solicitud no encontrada.' })
   async getRequestDocuments(@Param('id') requestId: string) {
     return this.requestsService.findDocumentsByRequest(requestId);
   }
