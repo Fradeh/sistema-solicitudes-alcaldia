102b1c09bfb1e285ff18a30a5147e4e54b51dfd0
Roderik Gonzalez <gonzalezroderik@gmail.com>
2026-05-20 21:29:53 -0500

    fix: add dependencies

--- backend/src/requests/requests.module.ts
+++ backend/src/requests/requests.module.ts
@@ -4,13 +4,22 @@ import { RequestsController } from './requests.controller';
 import { DocumentsModule } from '../documents/documents.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Request } from './entities/request.entity';
-import { Department } from 'src/departments/entities/department.entity';
+
+//Relasionships with other modules
+import { DepartmentsModule } from 'src/departments/departments.module';
+import { CategoriesModule } from 'src/categories/categories.module';
+import { RequestStatusesModule } from 'src/request-statuses/request-statuses.module';
+import { UsersModule } from 'src/users/users.module';
 
 
 
 @Module({
-  imports: [ TypeOrmModule.forFeature([Request, Department]), // Importa las entidades para TypeORM
+  imports: [ TypeOrmModule.forFeature([Request]), // Importa las entidades para TypeORM
     DocumentsModule, // conexión a MongoDB integrada
+    DepartmentsModule, // conexión al módulo de departamentos
+    CategoriesModule, // conexión al módulo de categorías
+    RequestStatusesModule, // conexión al módulo de estados de solicitudes
+    UsersModule, // conexión al módulo de usuarios
   ],
   controllers: [RequestsController],
   providers: [RequestsService],
