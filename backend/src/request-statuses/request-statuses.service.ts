152a5860987b7745955abba8d71670828aac2fa9
Lucas Santamaria <lucas.santamaria@utp.ac.pa>
2026-05-21 19:04:16 -0500

    feat: crear endpoint GET /api/v1/request-statuses (#23)

---
+++ backend/src/request-statuses/request-statuses.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { RequestStatus } from './entities/request-status.entity';
+
+@Injectable()
+export class RequestStatusesService {
+  constructor(
+    @InjectRepository(RequestStatus)
+    private readonly requestStatusRepository: Repository<RequestStatus>,
+  ) {}
+
+  findAll(): Promise<RequestStatus[]> {
+    return this.requestStatusRepository.find();
+  }
+}
